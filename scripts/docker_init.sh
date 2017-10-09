136c4b125b6b3da9782fbedd3304512149b96887
Alexander Baranov <sashab1@yandex-team.ru>
2017-10-09 17:01:34 +0300

    Cleanup

--- scripts/docker_init.sh
+++
@@ -1,7 +0,0 @@
-yum -y install sudo
-
-echo "export SHIPSOFT=/opt" >> /root/.bash_profile
-echo "export FAIRROOTPATH=\$SHIPSOFT/FairRootInst" >> /root/.bash_profile
-echo "export SIMPATH=\$SHIPSOFT/FairSoftInst" >> /root/.bash_profile
-echo "export FAIRSHIP=\$SHIPSOFT/FairShip" >> /root/.bash_profile
-echo "export PYTHONPATH=\$FAIRSHIP/python:\$SIMPATH/lib:\$SIMPATH/lib/Geant4:\$PYTHONPATH" >> /root/.bash_profile
